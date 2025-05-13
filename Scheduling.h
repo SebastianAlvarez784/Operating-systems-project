8089ef81ee49525cb04b24decacc39ab08c4236c
Sebastian Alvarez <sebalv@outlook.com>
2025-05-13 00:53:30 -0500

    Update Scheduling.h

--- Scheduling.h
+++ Scheduling.h
@@ -1,1 +1,13 @@
+#ifndef SCHEDULING_H
+#define SCHEDULING_H
+
+#include <vector>
+#include "Process.h"
+
+using namespace std;
+
+//  FIFO  function
+void FCFS(int& nextFreeFrame, int pageSize);
+
+#endif
 
